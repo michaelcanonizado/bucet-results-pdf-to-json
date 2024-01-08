0ef1eabaf11fbd4dbd79ee07b2b7f6ef511a70f5
rixmape <rixdonninorecario.mape@bicol-u.edu.ph>
2024-01-08 23:54:25 +0800

    Update to add utility functions in the main script
    Add campus and course code mapping
    Refactor directory structure and update data merging login

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-const startTime = performance.now();
-
-import { joinFormattedData } from "./utils/joinJsonData.js";
-import { convertAndFormatData } from "./utils/pdfToJson.js";
-
 import fs from "fs";
 import { stdin as input, stdout as output } from "node:process";
 import * as readline from "node:readline/promises";
+import { PdfReader } from "pdfreader";
 const rl = readline.createInterface({ input, output });
 
-import { consoleLog } from "./utils/consoleLog.js";
+let data = [];
+const mergedData = [];
 
 async function getFilesAndDirectories(dir) {
   return new Promise((resolve, reject) => {
@@ -21,31 +18,229 @@ async function getFilesAndDirectories(dir) {
 }
 
 async function scanAndFormatAllBucetResults(dirInput, dirOutput) {
-  consoleLog("READING");
+  // #1 - Loop through input directory to scan and convert all PDFs
+  const years = await getFilesAndDirectories(dirInput);
 
-  // #1 - Loop through 'data-pdf' directory to collect all Campus Abbreviations (e.g. /BUCS) and Course Code (e.g. /A-53) and scan, convert it to JSON, and format it one by one. Output the JSON in a folder "data-json" with the same file structure.
-  const campuses = await getFilesAndDirectories(dirInput);
+  for (const year of years) {
+    const campuses = await getFilesAndDirectories(`${dirInput}/${year}`);
 
-  for (const campus of campuses) {
-    const courseCodes = await getFilesAndDirectories(`${dirInput}/${campus}`);
+    for (const campus of campuses) {
+      const courseCodes = await getFilesAndDirectories(
+        `${dirInput}/${year}/${campus}`
+      );
 
-    for (const courseCode of courseCodes) {
-      await convertAndFormatData(dirInput, dirOutput, campus, courseCode);
+      for (const courseCode of courseCodes) {
+        await convertAndFormatData(
+          dirInput,
+          dirOutput,
+          year,
+          campus,
+          courseCode
+        );
+      }
     }
   }
-
-  // #2 - Loop through the output directory (data-json) and its sub-folders (e.g. /BUCS), and reformat, and  concatinate it all together in one JSON Object
-  await joinFormattedData(dirOutput, "./BUCET_RESULTS_2023_2024");
-
-  // COnsole log the elapsed time to monitor performance
-  const endTime = performance.now();
-  consoleLog("\n", "\n");
-  consoleLog(`Elapsed Time: ${(endTime - startTime).toFixed(3)}ms`);
-  consoleLog(`Elapsed Time: ${((endTime - startTime) / 1000).toFixed(3)}s`);
-  consoleLog("\n", "\n");
-
-  // Ensure CLI tool is terminated
+  // #2 - Loop through output directory to join all JSON files
+  await joinFormattedData(dirOutput, "./out/merged-data");
   rl.close();
 }
 
-scanAndFormatAllBucetResults("./data-pdf", "./data-json");
+async function joinJsonData(dir) {
+  await fs.readFile(dir, "utf8", (err, data) => {
+    if (err) return err;
+
+    mergedData.push(...JSON.parse(data));
+  });
+}
+
+export async function joinFormattedData(dirInput, dirOutput) {
+  const years = await getFilesAndDirectories(dirInput);
+
+  for (const year of years) {
+    const campuses = await getFilesAndDirectories(`${dirInput}/${year}`);
+
+    for (const campus of campuses) {
+      const courseCodes = await getFilesAndDirectories(
+        `${dirInput}/${year}/${campus}`
+      );
+
+      for (const courseCode of courseCodes) {
+        await joinJsonData(`${dirInput}/${year}/${campus}/${courseCode}`);
+      }
+    }
+  }
+
+  // #2.2 - Write the merged data to a single file.
+  await fs.writeFile(`${dirOutput}.json`, JSON.stringify(mergedData), (err) => {
+    if (err) {
+      return err;
+    }
+  });
+}
+
+function convertPdfToJson(dirInput) {
+  let pageData = {};
+
+  return new Promise(function (resolve, reject) {
+    // #1.1.1 - Scan the PDF
+    new PdfReader().parseFileItems(dirInput, (err, item) => {
+      if (err) {
+        console.log(err);
+        reject(err);
+      } else if (!item) {
+        reject(err);
+      } else if (item.page) {
+        // #1.1.3 - After each new page, push the page Object into the Array to organize the scanned PDF.
+        pageData = {};
+        data.push(pageData);
+        resolve();
+      } else if (item.text) {
+        // #1.1.2 - Store each scanned row into an Object where the keys are the Y-Value coordinates of the row.
+        (pageData[item.y] = pageData[item.y] || []).push(item.text);
+
+        resolve();
+      }
+    });
+  });
+}
+
+function sortObjectKeys(data) {
+  return data.map((dataPage) => {
+    const sortedObj = Object.keys(dataPage)
+      // #1.2.1 - Returns an array of sorted keys
+      .sort((a, b) => {
+        return parseFloat(a) - parseFloat(b);
+      })
+      // 1.2.2 - Uses the array of sorted keys to sort the Object by creating another Object as the accumulator. Using each item on the sorted keys Array as the 'key' for the Object, and also using each item on the sorted keys Array to set the value for each 'key' on the Object by matching the value to the dataPage Object.
+      .reduce((obj, key) => {
+        obj[key] = dataPage[key];
+        return obj;
+      }, {});
+
+    return sortedObj;
+  });
+}
+
+async function formatStudentData(data, YEAR, CAMPUS_ABV, COURSE_NUMBER) {
+  const studentsFormattedData = [];
+
+  for (const page of data) {
+    let studentStatus = null;
+
+    // #1.3.2 - Retrieve the status of the student.
+    for (const row in page) {
+      if (studentStatus === null) {
+        if (page[row][0].toUpperCase().trim() === "QUALIFIED") {
+          studentStatus = "QUALIFIED";
+        }
+        if (page[row][0].toUpperCase().trim() === "WAITLISTED") {
+          studentStatus = "WAITLISTED";
+        }
+      }
+    }
+
+    // #1.3.6 - Format all retrived data into one Student Object.
+    for (const row in page) {
+      // #1.3.6.1 = Determine a student when the first item of the Array is a number or float.
+      if (!isNaN(parseFloat(page[row][0]))) {
+        // #1.3.6.2 - Immediately disregard rows with less than 9 items.
+        if (page[row].length >= 8) {
+          // #1.3.6.3 - Students with complete values
+          if (page[row].length === 9) {
+            studentsFormattedData.push({
+              rank: page[row][0].trim(),
+              lastName: page[row][1].trim(),
+              firstName: page[row][2].trim(),
+              middleName: page[row][3].trim(),
+              city: page[row][4].trim(),
+              province: page[row][5].trim(),
+              school: page[row][6].trim(),
+              compositeRating: page[row][7].trim(),
+              percentileRank: page[row][8].trim(),
+              status: studentStatus.toUpperCase().trim(),
+              course: COURSE_NUMBER,
+              campus: CAMPUS_ABV,
+              year: YEAR,
+            });
+          } // #1.3.6.4 - Students with missing one value (no middle name).
+          else {
+            studentsFormattedData.push({
+              rank: page[row][0].trim(),
+              lastName: page[row][1].trim() || "",
+              firstName: page[row][2].trim(),
+              middleName: " ",
+              city: page[row][3].trim(),
+              province: page[row][4].trim(),
+              school: page[row][5].trim(),
+              compositeRating: page[row][6].trim(),
+              percentileRank: page[row][7].trim(),
+              status: studentStatus.toUpperCase().trim(),
+              course: COURSE_NUMBER,
+              campus: CAMPUS_ABV,
+              year: YEAR,
+            });
+          }
+        }
+      }
+    }
+  }
+
+  return studentsFormattedData;
+}
+
+async function writeData(dir, course, data) {
+  // #1.4.1 - Check whether the neccessary folders needed to maintain the folder structure of 'data-pdf' is present.
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir, { recursive: true });
+  }
+
+  // #1.4.2 - Once folder is present, write the file.json.
+  await fs.writeFile(
+    `${dir}/${course.replace(".pdf", ".json")}`,
+    JSON.stringify(data),
+    (err) => {
+      if (err) {
+        return err;
+      }
+    }
+  );
+}
+
+export async function convertAndFormatData(
+  dirInput,
+  dirOutput,
+  YEAR,
+  CAMPUS_ABV,
+  COURSE_NUMBER
+) {
+  // #1.1 - Locate and scan the PDF using the collected Campus Abbreviation and Course Code.
+  await convertPdfToJson(
+    `${dirInput}/${YEAR}/${CAMPUS_ABV}/${COURSE_NUMBER}`,
+    CAMPUS_ABV,
+    COURSE_NUMBER
+  );
+
+  // #1.2 - The data outputted by npm pdfreader is jumbled. Hence, the Object keys need to be arranged chronologically as their keys are the Y-Values of the scanned rows of the PDF.
+  const sortedData = sortObjectKeys(data);
+
+  // #1.3 - Format the data into an Array of Objects. Where each student is an Object and the keys represent the PDF table labels.
+  const studentsFormattedData = await formatStudentData(
+    sortedData,
+    YEAR,
+    CAMPUS_ABV,
+    COURSE_NUMBER
+  );
+
+  // #1.4 - Write the formatted data into a JSON file.
+  await writeData(
+    `${dirOutput}/${YEAR}/${CAMPUS_ABV}`,
+    COURSE_NUMBER,
+    studentsFormattedData
+  );
+
+  // #1.5 Flush the scanned and formatted students data for the next course, since the data is globally initialized
+  data = [];
+}
+
+// Run the script
+scanAndFormatAllBucetResults("./pdfs", "./out");
