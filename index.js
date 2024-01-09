ffa72e8d3c5d96977343f8ca6f07d3aa7c1ede2d
rixmape <rixdonninorecario.mape@bicol-u.edu.ph>
2024-01-09 08:32:44 +0800

    Remove sorting of data by rows

--- index.js
+++ index.js
@@ -16,61 +16,56 @@ async function readDirectory(path) {
 
 // Process PDF file and return data in a structured format.
 async function processPdf(path) {
-  let data = [];
-  let currentPageData = {};
+  let pages = [];
+  let currentPage = {};
 
   return new Promise((resolve, reject) => {
     new PdfReader().parseFileItems(path, (err, item) => {
       if (err) {
         reject(err);
       } else if (!item) {
-        resolve(data);
+        resolve(pages);
       } else if (item.page) {
-        currentPageData = {};
-        data.push(currentPageData);
+        currentPage = {};
+        pages.push(currentPage);
       } else if (item.text) {
-        (currentPageData[item.y] = currentPageData[item.y] || []).push(
-          item.text
-        );
+        (currentPage[item.y] = currentPage[item.y] || []).push(item.text);
       }
     });
   });
 }
 
-// Sort and format PDF data.
-function formatPdfData(data, path) {
-  const [year, campus, course] = path.split("/").slice(-3);
+// Format PDF data.
+function formatPdfData(data, year, campus, course) {
   let formattedData = [];
 
   data.forEach((page) => {
-    Object.keys(page)
-      .sort((a, b) => parseFloat(a) - parseFloat(b))
-      .forEach((y) => {
-        const rowData = page[y].map((item) => item.trim());
+    Object.keys(page).forEach((y) => {
+      const rowData = page[y].map((item) => item.trim());
 
-        // Fix: Status is always `null` because the status is always in the first row of the page. So, we need to traverse the array to find the status. Status either `QUALIFIED` or `WAITLISTED`
-        const status = ["QUALIFIED", "WAITLISTED"].find(
-          (s) => s === rowData[0].toUpperCase()
-        );
+      // Fix: Status is always `null` because the status is always in the first row of the page. So, we need to traverse the array to find thestatus.Status either`QUALIFIED`or`WAITLISTED`
+      const status = ["QUALIFIED", "WAITLISTED"].find(
+        (s) => s === rowData[0].toUpperCase()
+      );
 
-        if (!isNaN(parseFloat(rowData[0])) && rowData.length >= 8) {
-          formattedData.push({
-            rank: rowData[0],
-            lastName: rowData[1] || null,
-            firstName: rowData[2],
-            middleName: rowData.length === 9 ? rowData[3] : null,
-            city: rowData.length === 9 ? rowData[4] : rowData[3],
-            province: rowData.length === 9 ? rowData[5] : rowData[4],
-            school: rowData.length === 9 ? rowData[6] : rowData[5],
-            compositeRating: rowData.length === 9 ? rowData[7] : rowData[6],
-            percentileRank: rowData.length === 9 ? rowData[8] : rowData[7],
-            status: status,
-            course: course,
-            campus: campus,
-            year: year,
-          });
-        }
-      });
+      if (!isNaN(parseFloat(rowData[0])) && rowData.length >= 8) {
+        formattedData.push({
+          rank: rowData[0],
+          lastName: rowData[1] || null,
+          firstName: rowData[2],
+          middleName: rowData.length === 9 ? rowData[3] : null,
+          city: rowData.length === 9 ? rowData[4] : rowData[3],
+          province: rowData.length === 9 ? rowData[5] : rowData[4],
+          school: rowData.length === 9 ? rowData[6] : rowData[5],
+          compositeRating: rowData.length === 9 ? rowData[7] : rowData[6],
+          percentileRank: rowData.length === 9 ? rowData[8] : rowData[7],
+          status: status,
+          course: course,
+          campus: campus,
+          year: year,
+        });
+      }
+    });
   });
 
   return formattedData;
@@ -92,7 +87,13 @@ async function parsePdfFiles(inputPath, outputPath = "./output.json") {
         for (const course of courses) {
           const pdfPath = `${inputPath}/${year}/${campus}/${course}`;
           const pdfData = await processPdf(pdfPath);
-          const formattedData = formatPdfData(pdfData, pdfPath);
+          const formattedData = formatPdfData(
+            pdfData,
+            pdfPath,
+            year,
+            campus,
+            course
+          );
           allData.push(...formattedData);
         }
       }
